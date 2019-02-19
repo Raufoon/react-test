e0374e262e628cdc1919e6fbe22062b3b19ea69c
Raufoon <minhazraufooncsedu@gmail.com>
2019-02-19 13:25:53 +0600

    feat: api for get all posts

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,14 @@
-// use axios here
+import axios from 'axios';
+
+
+const SERVER_URL = 'https://my-json-server.typicode.com/Raufoon/react-test'
+
+const fetchAllPosts = async () => {
+  let postsDTO;
+  try {
+    postsDTO = await axios.get(`${SERVER_URL}\posts`)
+    return JSON.parse(postsDTO);
+  } catch (error) {
+    console.error(error)
+  }
+};
